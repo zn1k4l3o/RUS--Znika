c940a7db0addd7bd9005026d434a7ffcd49e613f
zn1k4l3o <leo.znika@tvz.hr>
2026-03-24 11:06:18 +0100

    feat: documentation

--- Lab1/lab1.ino
+++ Lab1/lab1.ino
@@ -1,32 +1,95 @@
+/**
+ * @file main.ino
+ * @brief Demonstracija rada s višestrukim prekidima i prioritetima
+ *
+ * Sustav implementira:
+ * - tri tipkala s različitim prioritetima (HIGH, MID, LOW)
+ * - periodički timer prekid
+ * - senzor (digitalni ulaz)
+ *
+ * Prekidi postavljaju zastavice koje se obrađuju u glavnoj petlji.
+ * Prioriteti se implementiraju softverski u funkciji handleInterrupts().
+ *
+ * @author 
+ * @date 2026
+ */
+
+/** @name LED pinovi */
+//@{
 #define LED_CHECK0 4
 #define LED_CHECK1 5
 #define LED_CHECK2 6
 #define LED_CHECK3 7
+//@}
 
-#define BUTTON_INPUT0 12
-#define BUTTON_INPUT1 13
-#define BUTTON_INPUT2 14
+/** @name Ulazni pinovi */
+//@{
+#define BUTTON_INPUT0 12 ///< Tipkalo - visoki prioritet
+#define BUTTON_INPUT1 13 ///< Tipkalo - srednji prioritet
+#define BUTTON_INPUT2 14 ///< Tipkalo - niski prioritet
+#define SENSOR_INPUT 15  ///< PIR senzor
+//@}
 
-#define SENSOR_INPUT 15
-
+/**
+ * @brief Zastavice za obradu prekida
+ *
+ * Svaki indeks predstavlja jedan izvor prekida:
+ * 0 - HIGH, 1 - MID, 2 - LOW
+ */
 volatile bool actionFlag[3] = {false, false, false};
+
+/**
+ * @brief Vrijeme zadnjeg prekida za debounce
+ */
 volatile unsigned long lastInterruptTime[3] = {0, 0, 0};
+
+/// Zadnje vrijeme aktivacije timera
 volatile unsigned long lastTimerTime = 0;
+
+/// Interval blinkanja LED-a (ms)
 const unsigned long BLINK_INTERVAL = 100;
+
+/// Debounce vrijeme za tipkala (ms)
 const int DEBOUNCE_DELAY = 50;
+
+/// Period timera (ms)
 const int TIMER_DELAY = 1000;
-const int ALARM_DISTANCE = 100;
+
+/// Trajanje LED indikacije (ms)
 const int LED_DURATION = 1000;
 
-volatile bool interruptInProgress = false;   
+/**
+ * @brief Oznaka da je obrada prekida u tijeku
+ *
+ * Koristi se za sprječavanje konflikata između ISR i glavne petlje.
+ */
+volatile bool interruptInProgress = false; 
 
+/** 
+ * Timer handler i zastavica za označavanje da se obraduje timer prekid
+ */
 hw_timer_t *timer = NULL;
 volatile bool timerFlag = false;
 
+/**
+ * @brief Timer ISR
+ *
+ * Aktivira se periodički i postavlja zastavicu za obradu senzora.
+ */
 void IRAM_ATTR onTimer() {
   timerFlag = true; 
 }
 
+/**
+ * @brief Inicijalizacija sustava
+ *
+ * Postavlja:
+ * - pinove
+ * - tipkala
+ * - prekide za tipkala
+ * - timer prekid
+ * - serijsku komunikaciju
+ */
 void setup() {
   pinMode(LED_CHECK0, OUTPUT);
   pinMode(LED_CHECK1, OUTPUT);
@@ -52,6 +115,14 @@ void setup() {
   Serial.println("Start");
 }
 
+/**
+ * @brief Glavna petlja programa
+ *
+ * Redoslijed:
+ * 1. Obrada timer događaja (senzor) ukoliko je zastavica postavljena
+ * 2. Obrada prekida prema prioritetu
+ * 3. Idle stanje (blink LED)
+ */
 void loop() {
 
   if (timerFlag) {
@@ -67,6 +138,11 @@ void loop() {
 
 }
 
+/**
+ * @brief Obrada podataka senzora
+ *
+ * Poziva se nakon timer prekida.
+ */
 void handleSensorUpdate() {
   int result = digitalRead(SENSOR_INPUT);
   if (result) {
@@ -75,6 +151,16 @@ void handleSensorUpdate() {
   timerFlag = false;
 }
 
+/**
+ * @brief Obrada prekida prema prioritetu
+ *
+ * Prioritet:
+ * 1. HIGH (index 0)
+ * 2. MID (index 1)
+ * 3. LOW (index 2)
+ *
+ * Koristi else-if kako bi se osiguralo izvršavanje samo jednog prekida.
+ */
 void handleInterrupts() {
   interruptInProgress = true;
 
@@ -94,24 +180,40 @@ void handleInterrupts() {
   interruptInProgress = false;
 }
 
+/**
+ * @brief ISR za tipkalo visokog prioriteta
+ */
 void HIGH_INTERRUPT() {
   if (!interruptInProgress) {
     handleInterrupt(0, "HIGH Priority");
   }
 }
 
+/**
+ * @brief ISR za tipkalo srednjeg prioriteta
+ */
 void MID_INTERRUPT() {
   if (!interruptInProgress) {
     handleInterrupt(1, "MID Priority");
   }
 }
 
+/**
+ * @brief ISR za tipkalo niskog prioriteta
+ */
 void LOW_INTERRUPT() {
   if (!interruptInProgress) {
     handleInterrupt(2, "LOW Priority");
   }
 }
 
+/**
+ * @brief Blinkanje LED-a
+ *
+ * @param ledPin Pin LED-a
+ *
+ * Funkcija za treperenje LED svjetla.
+ */
 void blinkLed(int ledPin) {
   unsigned long startTime = millis();
   while (millis() - startTime < LED_DURATION) {
@@ -121,6 +223,16 @@ void blinkLed(int ledPin) {
   digitalWrite(ledPin, LOW);
 }
 
+/**
+ * @brief Obrada prekida (zajednička funkcija)
+ *
+ * @param index Indeks prekida (0-2)
+ * @param message Poruka za serijski izlaz
+ *
+ * Implementira:
+ * - debounce zaštitu
+ * - postavljanje zastavice
+ */
 void handleInterrupt(int index, const char* message) {
   if (millis() - lastInterruptTime[index] < DEBOUNCE_DELAY) {
     return;
